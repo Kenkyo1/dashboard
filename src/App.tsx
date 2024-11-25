ad99c8222400cbe0d2f46b416ca76c0164a79e0a
Kenkyo1 <anargome@espol.edu.ec>
2024-11-25 13:45:39 -0500

    guia 12 first try

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import './App.css'
 import Grid from '@mui/material/Grid2'
+import IndicatorWeather from './components/IndicatorWeather';
+
 
 function App() {
 
@@ -7,16 +9,26 @@ function App() {
     <Grid container spacing={5}>
 
       {/* Indicadores */}
-      <Grid size={{xs: 12 , xl:3}}>Elemento: Indicador 1</Grid>
-      <Grid size={{xs: 12 , xl:3}}>Elemento: Indicador 2</Grid>
-      <Grid size={{xs: 12 , xl:3}}>Elemento: Indicador 3</Grid>
-      <Grid size={{xs: 12 , xl:3}}>Elemento: Indicador 4</Grid>
+
+
+      <Grid size={{ xs: 12, xl: 3 }}>
+        <IndicatorWeather title={'Indicador 1'} subtitle={'Unidad 1'} value={'1.23'} />
+      </Grid>
+      <Grid size={{ xs: 12, xl: 3 }}>
+        <IndicatorWeather title={'Indicador 2'} subtitle={'Unidad 2'} value={'3.12'} />
+      </Grid>
+      <Grid size={{ xs: 12, xl: 3 }}>
+        <IndicatorWeather title={'Indicador 3'} subtitle={'Unidad 3'} value={'2.31'} />
+      </Grid>
+      <Grid size={{ xs: 12, xl: 3 }}>
+        <IndicatorWeather title={'Indicador 4'} subtitle={'Unidad 4'} value={'3.21'} />
+      </Grid>
 
       {/* TABLA */}
-      <Grid size={{xs:12 , xl: 8 }}>Elemento: Tabla</Grid>
+      <Grid size={{ xs: 12, xl: 8 }}>Elemento: Tabla</Grid>
 
       {/* Gráfico */}
-      <Grid size={{xs:12 , xl: 4 }}>Elemento: Gráfico 1</Grid>
+      <Grid size={{ xs: 12, xl: 4 }}>Elemento: Gráfico 1</Grid>
 
     </Grid>
   )
